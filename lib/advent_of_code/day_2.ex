b43f1414ba518fda35c5263128972dedd076c34d
Ryan LaBouve <imryanlabouve@gmail.com>
2019-12-02 01:36:27 -0600

    Why did I stay up, refactor, and make this concurrent :rip:

--- lib/advent_of_code/day_2.ex
+++ lib/advent_of_code/day_2.ex
@@ -1,24 +1,58 @@
 defmodule AdventOfCode.Day2 do
-  def run do
+  @instruction_length 4
+
+  def run_part_1 do
+    IO.puts("Running part 1")
+    run_from_file(12, 2)
+  end
+
+  def run_part_2() do
+    IO.puts("Running part 2")
+
+    # oh my. this is v rough
+    # but ideally it executes the search in parallel
+    # spawning off an ungoodly amount of backgroung processes
+    # and simply erroring when it find what I'm looking for
+    {v, noun, verb} =
+      Enum.map(0..99, fn noun ->
+        List.duplicate(noun, 99)
+        |> Enum.zip(0..99)
+      end)
+      |> List.flatten()
+      |> Enum.map(fn {noun, verb} ->
+        fn -> run_part_2(noun, verb) end
+      end)
+      |> Enum.map(&Task.async(fn -> &1.() end))
+      |> Enum.map(&Task.await/1)
+      |> Enum.find(fn {v, _noun, _verb} -> v == 19_690_720 end)
+
+    IO.puts("Found #{v} at n#{noun}v#{verb}")
+  end
+
+  def run_part_2(noun, verb) do
+    # if Enum.at(run_from_file(noun, verb), 0) == 19_690_720 do
+    #   raise "Found match at n#{noun}v#{verb}"
+    # end
+    {Enum.at(run_from_file(noun, verb), 0), noun, verb}
+  end
+
+  def run_from_file(noun \\ 12, verb \\ 2) do
     with {:ok, file_string} <- File.read("lib/advent_of_code/day_2_input.txt") do
       file_string
       |> String.split(",")
       |> Enum.map(&String.to_integer(&1))
-      |> run
+      |> run_with_pos(noun, verb)
     end
   end
 
-  def run(prog) do
-    # Initial replace at bottom of instructions
+  def run_with_pos(prog, noun, verb) do
     prog =
       prog
-      |> List.replace_at(1, 12)
-      |> List.replace_at(2, 2)
+      |> List.replace_at(1, noun)
+      |> List.replace_at(2, verb)
 
     run(prog, 0)
   end
-
-  @instruction_length 4
 
   def run(prog, cursor) do
     run(prog, cursor, Enum.slice(prog, cursor, 4))
@@ -39,20 +73,11 @@ defmodule AdventOfCode.Day2 do
   end
 
   def run(prog, _cursor, [99 | _]) do
-    IO.inspect(prog)
-    IO.puts("HALT")
+    # IO.inspect(prog)
+    prog
   end
 
-  def run(_prog, _cursor, _) do
-    raise "Unknown Opcode"
+  def run(_prog, _cursor, [opcode | _]) do
+    raise "Unknown Opcode #{opcode}"
   end
-
-  # def swap(prog, pos1, pos2) do
-  #   with val1 <- Enum.at(prog, pos1),
-  #        val2 <- Enum.at(prog, pos2) do
-  #     prog
-  #     |> List.replace_at(pos1, val2)
-  #     |> List.replace_at(pos2, val1)
-  #   end
-  # end
 end
